b7d907b70ed4fa3c1b30f8e73071658c1d3f297a
jesse.hunter <jesse.hunter311@yahoo.com>
2020-07-05 11:51:30 -0400

    adds reservation form

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const result = document.getElementById('result');
 const btn = document.getElementById('btn');
 
@@ -9,7 +7,6 @@ function getRandomBrewery() {
 	fetch('https://api.openbrewerydb.org/breweries?by_state=kentucky')
 		.then(res => res.json())
 		.then(data => {
-			console.log(data);
 			let brew = data[Math.floor(Math.random() * data.length)];
 			result.innerHTML = `${brew.name}<br />
 			 -<br />
@@ -19,10 +16,27 @@ function getRandomBrewery() {
 			 ${brew.street}<br />
 			 -<br />
 			 <a target="_blank" rel="noopener noreferrer" href='${brew.website_url}'>Visit Site</a>`;
-			})
-			.catch((error) => {
-				console.error('Error:', error);    
+		})
+		.catch((error) => {
+			console.error('Error:', error);
 		});
 }
 
 
+let modal = document.getElementById("myModal");
+let modalBtn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+
+modalBtn.onclick = function () {
+	modal.style.display = "block";
+}
+
+span.onclick = function () {
+	modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
